a4321c1d7be5778567b36a65c66714225276b39e
Durev <vincent.durewski@gmail.com>
2018-03-23 14:31:12 +0100

    Add show view for currency resource

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -3,5 +3,9 @@ class CurrenciesController < ApplicationController
     @currencies = Currency.order(:rank)
   end
 
+  def show
+    @currency = Currency.find(params[:id])
+  end
+
   def details; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'currencies#index'
   get 'currencies/details'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :currencies, only: %i(index show)
 end
