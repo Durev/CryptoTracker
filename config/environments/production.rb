9000adf232b9b97e6084062ac22728acec14f34e
Durev <vincent.durewski@gmail.com>
2018-03-25 20:20:02 +0200

    Set host parameter for mailer (with env var for staging and prod)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { host: ENV['MAILER_URL'] }
 end
